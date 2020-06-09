964bfa7c775070a202136b662123ab04f64c64cf
nikita1612 <66557680+nikita1612@users.noreply.github.com>
2020-06-09 12:59:34 +0530

    Add files via upload

---
+++ accessing element from tuple.py
@@ -0,0 +1,2 @@
+x=(1,2,3,4,5)
+print("second position number is:",x[1])
---
+++ deleting element from dictionary.py
@@ -0,0 +1,5 @@
+print("IQ of 3 students")
+y = {"Anu":134,"Seshu":87,"Riya":115}
+print(y)
+del y["Seshu"]
+print("IQ of 2 students",y)
